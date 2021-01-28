7572730f01e5ddf5c04f5dcb1a2d7cfd58af1093
Buxy <47367285+buxy-xyz@users.noreply.github.com>
2021-01-28 15:18:35 +0100

    colorWhite fix
    removed FOREGROUND_INTENSITY

--- main.h
+++ main.h
@@ -8,4 +8,4 @@
 #define colorGreen 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN)
 #define colorBlue 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE)
 #define colorYellow			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN)
-#define colorWhite			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE)
+#define colorWhite			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE)
